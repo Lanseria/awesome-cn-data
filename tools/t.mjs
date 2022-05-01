485a40e7bba5a3b6eba98ebe96efa90c217e7303
lanseria <zhangchao564265135@hotmail.com>
2022-05-01 20:39:07 +0800

    update: zhoushan dinghai data

--- tools/t.mjs
+++ tools/t.mjs
@@ -1,7 +1,7 @@
 import gcoord from 'gcoord';
 
 const loc = gcoord.transform(
-  [122.154771,30.067237],    // 经纬度坐标
+  [122.06223,30.112168],    // 经纬度坐标
   gcoord.GCJ02,               // 当前坐标系
   gcoord.WGS84                 // 目标坐标系
 );
