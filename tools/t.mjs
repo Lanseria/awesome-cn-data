8ca71a7ba28844d56af11afbfa407b817356a513
lanseria <zhangchao564265135@hotmail.com>
2022-05-03 19:14:33 +0800

    chore: update zhoushan putuo data

--- tools/t.mjs
+++ tools/t.mjs
@@ -1,7 +1,7 @@
 import gcoord from 'gcoord';
 
 const loc = gcoord.transform(
-  [122.215019,29.974566],    // 经纬度坐标
+  [122.240617,29.998534],    // 经纬度坐标
   gcoord.GCJ02,               // 当前坐标系
   gcoord.WGS84                 // 目标坐标系
 ); 
