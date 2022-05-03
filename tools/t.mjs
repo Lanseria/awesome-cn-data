79c32bf575f146eaa5685dd2e1092fdb8feb1c96
lanseria <zhangchao564265135@hotmail.com>
2022-05-03 14:59:55 +0800

    chore:  update zhoushan lincheng data

--- tools/t.mjs
+++ tools/t.mjs
@@ -1,7 +1,7 @@
 import gcoord from 'gcoord';
 
 const loc = gcoord.transform(
-  [121.989156,30.112316],    // 经纬度坐标
+  [122.215019,29.974566],    // 经纬度坐标
   gcoord.GCJ02,               // 当前坐标系
   gcoord.WGS84                 // 目标坐标系
 ); 
